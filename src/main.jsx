7676c062d04d5f2e2a7a802097c0569d6da03089
hongmikyeong <mikyeong.dev@gmail.com>
2023-09-21 10:39:11 +0900

    react query 설정 적용

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </QueryClientProvider>
 );
