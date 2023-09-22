1b3591b8af7a3f8b7b2a9d02cae9548d53e4c3ae
hongmikyeong <mikyeong.dev@gmail.com>
2023-09-22 16:53:31 +0900

    tailwind 설치

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 import { QueryClient, QueryClientProvider, useQuery } from "react-query";
+import "./index.css";
+import "tailwindcss/tailwind.css";
 
 const queryClient = new QueryClient();
 
