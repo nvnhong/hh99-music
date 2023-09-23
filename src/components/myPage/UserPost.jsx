9ee35c5bc23d7f897a31aa63cf7e2f8ae1948ab1
hongmikyeong <mikyeong.dev@gmail.com>
2023-09-23 19:47:00 +0900

    마이페이지 글목록 보기 기능 추가

--- src/components/myPage/UserPost.jsx
+++ src/components/myPage/UserPost.jsx
@@ -2,10 +2,18 @@ import { useNavigate } from "react-router-dom";
 import * as St from "../../styles/Styles";
 import Header from "../common/Header";
 import List from "./List";
+import { useQuery } from "react-query";
+import { getMyPost } from "../../api/api";
+import { useSelector } from "react-redux";
 
 export default function UserPost() {
   const navigate = useNavigate();
-  const data = ["글 제목 1", "글 제목 2", "글 제목 3"];
+  const { userId } = useSelector((state) => state.user);
+  const { isLoading, error, data } = useQuery("post", () => getMyPost(userId));
+
+  if (isLoading) {
+    return <div>로딩중</div>;
+  }
 
   return (
     <St.Container>
@@ -16,7 +24,14 @@ export default function UserPost() {
           내가 작성한 게시글 목록
         </St.List>
         {data.length > 0 ? (
-          data.map((value, index) => <List key={index} title={value} />)
+          data.map((value) => (
+            <List
+              key={value.id}
+              id={value.id}
+              title={value.title}
+              handleClick={() => navigate("/post", { state: value.id })}
+            />
+          ))
         ) : (
           <St.UserPostForm>작성된 글이 없습니다.</St.UserPostForm>
         )}
